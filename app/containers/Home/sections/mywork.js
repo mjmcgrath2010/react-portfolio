eb9c09d2829b63233e9623c90db70fcff7b7d9c9
Michael McGrath <mike@mcgrathsolutions.us>
2017-12-12 19:29:12 -0500

    HapYak Logo Added

--- app/containers/Home/sections/mywork.js
+++ app/containers/Home/sections/mywork.js
@@ -9,35 +9,48 @@ import OSHome from '../../../images/projects/oppspace/opportunityspacehome.png';
 import Residential from '../../../images/projects/oppspace/residential.png';
 import HapYakAcademy from '../../../images/projects/hapyak/hapyak-academy.png';
 import CoreReports from '../../../images/projects/hapyak/core-reports.png';
+import HapYakLogo from '../../../images/logos/hapyak_logo_color.png';
 
 function MyWork() {
   return (
     <Wrapper>
       <Grid>
-        <h2>My Work</h2>
+        <Grid.Row columns={1}>
+          <Grid.Column>
+            <h2>My Work</h2>
+          </Grid.Column>
+        </Grid.Row>
+        <img className="logo" alt="hapyak-logo" src={HapYakLogo} />
         <Grid.Row columns={3}>
           <Grid.Column>
             <ProjectThumbnail
-              projectPage="/opportunity-space-home-page"
+              projectPage="/projects/hapyak-reporting"
+              altTag="HapYak Core Reports"
+              thumbnail={CoreReports}
+            />
+          </Grid.Column>
+          <Grid.Column>
+            <ProjectThumbnail
+              projectPage="/projects/hapyak-academy"
+              altTag="HapYak Academy"
+              thumbnail={HapYakAcademy}
+            />
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row columns={3}>
+          <Grid.Column>
+            <ProjectThumbnail
+              projectPage="/projects/opportunity-space-home-page"
               altTag="OpportunitySpace Home Page"
               thumbnail={OSHome}
             />
           </Grid.Column>
           <Grid.Column>
             <ProjectThumbnail
-              projectPage="/opportunity-space-marketing"
+              projectPage="/projects/opportunity-space-marketing"
               altTag="OpportunitySpace Marketing Campaigns"
               thumbnail={Residential}
             />
-          </Grid.Column>
-        </Grid.Row>
-        <h3>HapYak</h3>
-        <Grid.Row columns={3}>
-          <Grid.Column>
-            <ProjectThumbnail projectPage="/hapyak-reporting" altTag="HapYak Core Reports" thumbnail={CoreReports} />
-          </Grid.Column>
-          <Grid.Column>
-            <ProjectThumbnail projectPage="/hapyak-academy" altTag="HapYak Academy" thumbnail={HapYakAcademy} />
           </Grid.Column>
         </Grid.Row>
       </Grid>
