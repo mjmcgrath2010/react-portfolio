1a0cea250747c522d1356c1e12e75d025e947896
Michael McGrath <mike@mcgrathsolutions.us>
2018-01-02 18:31:04 -0500

    Adding Drop me a line

--- app/containers/Home/sections/contact.js
+++ app/containers/Home/sections/contact.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { Grid } from 'semantic-ui-react';
 import { Wrapper } from '../styles';
+import ContactForm from '../../../components/ContactForm/index';
 // import PropTypes from 'prop-types';
 
 function Contact() {
   return (
     <Wrapper>
       <h2>Drop me a line</h2>
+      <Grid columns={1}>
+        <Grid.Row>
+          <Grid.Column>
+            <ContactForm />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
     </Wrapper>
   );
 }
