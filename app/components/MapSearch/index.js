dced452d9f06fc6c0a8a93ec767fa336e53f5efe
Michael McGrath <mike@mcgrathsolutions.us>
2018-01-07 15:19:34 -0500

    Added dynamic markers

--- app/components/MapSearch/index.js
+++ app/components/MapSearch/index.js
@@ -14,11 +14,25 @@ import { FormattedMessage } from 'react-intl';
 import PrimaryPin from '../../images/icons/map-localization.png';
 import messages from './messages';
 import MapSearchBar from '../MapSearchBar/index';
+
+const mikesIcon = L.icon({
+  iconUrl: PrimaryPin,
+  iconSize: [40, 40], // size of the icon
+  iconAnchor: [20, 40], // point of the icon which will correspond to marker's location
+  shadowAnchor: [4, 62], // the same for the shadow
+  popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
+});
+
+let mymap;
+
 // eslint-disable-next-line react/prefer-stateless-function
 class MapSearch extends React.PureComponent {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      pins: [],
+      center: [42.342813, -71.097606],
+    };
   }
   componentDidMount() {
     this.setUpMap();
@@ -30,35 +44,34 @@ class MapSearch extends React.PureComponent {
       'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
       '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
       'Imagery © <a href="http://mapbox.com">Mapbox</a>';
-    const mikesIcon = L.icon({
-      iconUrl: PrimaryPin,
-      iconSize: [40, 40], // size of the icon
-      iconAnchor: [20, 40], // point of the icon which will correspond to marker's location
-      shadowAnchor: [4, 62], // the same for the shadow
-      popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
-    });
     const satellite = L.tileLayer(mapBoxUrl, {
       id: 'mapbox.satellite',
       attribution: mbAttr,
     });
     const streets = L.tileLayer(mapBoxUrl, { id: 'mapbox.streets', attribution: mbAttr });
-    const myHome = L.marker([42.342813, -71.097606], { icon: mikesIcon }).bindPopup('My Home');
-    const cities = L.layerGroup([myHome]);
-    const mymap = L.map('mapid', {
-      center: [42.342813, -71.097606],
+
+    this.state.pins.push(L.marker([42.342813, -71.097606], { icon: mikesIcon }).bindPopup('My Home'));
+    const pins = L.layerGroup(this.state.pins);
+    mymap = L.map('mapid', {
+      center: this.state.center,
       zoom: 15,
-      layers: [cities, streets],
+      layers: [pins, streets],
     });
     const baseMaps = {
       Streets: streets,
       Satellite: satellite,
     };
-
     const overlayMaps = {
-      Cities: cities,
+      Pins: this.state.pins,
     };
-
     L.control.layers(baseMaps, overlayMaps).addTo(mymap);
+  }
+  mapLocation(location) {
+    const lat = location.geometry.location.lat();
+    const lng = location.geometry.location.lng();
+    const newPin = L.marker([lat, lng], { icon: mikesIcon }).bindPopup('My Home');
+    this.state.pins.push(newPin);
+    newPin.addTo(mymap);
   }
   render() {
     return (
@@ -76,7 +89,10 @@ class MapSearch extends React.PureComponent {
               <div id="mapid" />
             </Grid.Column>
             <Grid.Column width={6}>
-              <MapSearchBar />
+              <MapSearchBar
+                // eslint-disable-next-line react/jsx-no-bind
+                submitLocation={this.mapLocation.bind(this)}
+              />
             </Grid.Column>
           </Grid.Row>
         </Grid>
