b04c58ffc77bcbf7cb83a8df0bf6b236de5efd20
Michael McGrath <mike@mcgrathsolutions.us>
2018-01-07 22:03:33 -0500

    Fixing things

---
+++ app/components/Charts/messages.js
@@ -0,0 +1,13 @@
+/*
+ * Charts Messages
+ *
+ * This contains all the text for the Charts component.
+ */
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  header: {
+    id: 'app.components.Charts.header',
+    defaultMessage: 'This is the Charts component !',
+  },
+});
