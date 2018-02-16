18070fbbe3e02c5c06f382a909db6f64d9e7deca
Michael McGrath <mike@mcgrathsolutions.us>
2018-02-16 06:30:53 -0500

    Cleaned up API

--- server/api/geolocate.js
+++ server/api/geolocate.js
@@ -5,18 +5,13 @@
 const GooglePlaces = require('google-places');
 const places = new GooglePlaces('AIzaSyCjXddPanpmLwtsDoXLHNqwhiEmCtMlc0U');
 
-module.exports = (req, res) => {
-  // places.search({ keyword: req.query.search }, (searchErr, response) => {
-  //   places.details({ reference: response.results[0].reference }, (detailErr, detailResponse) => {
-  //     if (detailResponse) {
-  //       res.send(detailResponse);
-  //     }
-  //   });
-  // });
-
-  places.autocomplete({ input: req.query.keyword, types: 'address' }, (autoCompleteErr, response) => {
-    if (response) {
-      res.send(response);
-    }
-  });
+module.exports = {
+  autoComplete: (req, res) => {
+    places.autocomplete({ input: req.query.keyword, types: 'address' }, (autoCompleteErr, response) => {
+      if (autoCompleteErr) {
+        return res.status(400).send(autoCompleteErr);
+      }
+      return res.status(200).send(response);
+    });
+  },
 };
--- server/index.js
+++ server/index.js
@@ -10,10 +10,10 @@ const isDev = process.env.NODE_ENV !== 'production';
 const ngrok = (isDev && process.env.ENABLE_TUNNEL) || argv.tunnel ? require('ngrok') : false;
 const resolve = require('path').resolve;
 const app = express();
-const geoLocate = require('./api/geolocate');
+const api = require('./api/geolocate');
 
-// If you need a backend, e.g. an API, add your custom backend-specific middleware here
-app.use('/api/geolocate', geoLocate);
+// Backend api and middleware
+app.use('/api/geolocate', api.autoComplete);
 
 // In production we need to pass these values in instead of relying on webpack
 setup(app, {
