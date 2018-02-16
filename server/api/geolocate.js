89dbea9a6faa81582a6c3c3aec4efd4562aacd4a
mjmcgrath2010 <mike@mcgrathsolutions.us>
2018-02-15 22:22:53 -0500

    Serving up backend

--- server/api/geolocate.js
+++ server/api/geolocate.js
@@ -2,29 +2,19 @@
  * Map Utils
  */
 
-var GooglePlaces = require('google-places');
-var places = new GooglePlaces('AIzaSyCjXddPanpmLwtsDoXLHNqwhiEmCtMlc0U');
+const GooglePlaces = require('google-places');
+const places = new GooglePlaces('AIzaSyCjXddPanpmLwtsDoXLHNqwhiEmCtMlc0U');
 
-module.exports = (req, res, next) => {
-  // const geoLocate = input => {
-  //   const key = 'AIzaSyCjXddPanpmLwtsDoXLHNqwhiEmCtMlc0U';
-  //   const baseUrl = `https://maps.googleapis.com/maps/api/place/autocomplete/json?types=establishment&location=42.342813,-71.097606&radius=500&key=${
-  //     key
-  //   }&input=${input}`;
-  // };
+module.exports = (req, res) => {
+  // places.search({ keyword: req.query.search }, (searchErr, response) => {
+  //   places.details({ reference: response.results[0].reference }, (detailErr, detailResponse) => {
+  //     if (detailResponse) {
+  //       res.send(detailResponse);
+  //     }
+  //   });
+  // });
 
-  places.search({ keyword: req.query.keyword }, function(err, response) {
-    console.log('search: ', response.results);
-
-    places.details({ reference: response.results[0].reference }, function(err, response) {
-      console.log('search details: ', response.result.website);
-      // search details:  http://www.vermonster.com/
-    });
-  });
-
-  places.autocomplete({ input: 'Verm', types: '(cities)' }, function(err, response) {
-    console.log('autocomplete: ', response.predictions);
-
+  places.autocomplete({ input: req.query.keyword, types: 'address' }, (autoCompleteErr, response) => {
     if (response) {
       res.send(response);
     }
