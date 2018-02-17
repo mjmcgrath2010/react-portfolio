66cb4ccc415475d1e5a54bca35aadaa5482837c3
Michael McGrath <mike@mcgrathsolutions.us>
2018-02-17 15:20:41 -0500

    Deploying

--- server/api/index.js
+++ server/api/index.js
@@ -2,20 +2,13 @@
  *  API and Middleware methods
  */
 
-const GooglePlaces = require('google-places');
-const places = new GooglePlaces('AIzaSyCjXddPanpmLwtsDoXLHNqwhiEmCtMlc0U');
 const googleMapsClient = require('@google/maps').createClient({
   key: 'AIzaSyCjXddPanpmLwtsDoXLHNqwhiEmCtMlc0U',
 });
 
 module.exports = {
   autoComplete: (req, res) => {
-    places.autocomplete({ input: req.query.keyword, types: 'address' }, (autoCompleteErr, response) => {
-      if (autoCompleteErr) {
-        return res.status(400).send(autoCompleteErr);
-      }
-      return res.status(200).send(response);
-    });
+    res.send(400);
   },
   geocode: (req, res) => {
     googleMapsClient.geocode(
