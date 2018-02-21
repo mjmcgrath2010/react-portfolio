249ce90c3ab717593775583b378aed32d842067b
Michael McGrath <mike@mcgrathsolutions.us>
2018-02-20 20:12:18 -0500

    Making progress

--- app/components/Charts/index.js
+++ app/components/Charts/index.js
@@ -5,7 +5,11 @@
  */
 
 import React from 'react';
+import _ from 'lodash';
+import request from '../../utils/request';
+
 const Chart = require('chart.js');
+
 // import styled from 'styled-components';
 
 // import { FormattedMessage } from 'react-intl';
@@ -15,52 +19,74 @@ const Chart = require('chart.js');
 class Charts extends React.PureComponent {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      data: null,
+      title: null,
+    };
   }
   componentDidMount() {
     const ctx = document.getElementById('myChart');
-    const myChart = new Chart(ctx, {
-      type: 'bar',
-      data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [
-          {
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-              'rgba(255, 159, 64, 0.2)',
-            ],
-            borderColor: [
-              'rgba(255,99,132,1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-              'rgba(153, 102, 255, 1)',
-              'rgba(255, 159, 64, 1)',
-            ],
-            borderWidth: 1,
-          },
-        ],
-      },
-      options: {
-        scales: {
-          yAxes: [
+    const that = this;
+
+    request('/stock-data?symbol=AAPL&interval=1')
+      .then(response => {
+        const title = response['Meta Data']['1. Information'];
+        const stockData = response['Time Series (1min)'];
+        const processedData = [];
+        if (stockData) {
+          _.each(stockData, stock => {
+            processedData.push(stock);
+          });
+        }
+        this.setState({
+          data: processedData,
+          title,
+        });
+        console.log(processedData);
+        renderChart();
+      })
+      .catch(err => console.log(err));
+    const renderChart = () => {
+      const myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+          datasets: [
             {
-              ticks: {
-                beginAtZero: true,
-              },
+              label: that.state.title,
+              data: that.state.data,
+              backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+              ],
+              borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+              ],
+              borderWidth: 1,
             },
           ],
         },
-        responsive: true,
-      },
-    });
-    return myChart;
+        options: {
+          scales: {
+            yAxes: [
+              {
+                ticks: {
+                  beginAtZero: true,
+                },
+              },
+            ],
+          },
+          responsive: true,
+        },
+      });
+      return myChart;
+    };
   }
   render() {
     return (
