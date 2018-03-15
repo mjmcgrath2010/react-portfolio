9b9ddc5254a036a4f2750f6efe69952bbfb15347
Mike McGrath <mike@mikemcgrathonline.us>
2018-03-14 21:47:44 -0400

    Restructuring styles

--- app/global-styles.js
+++ app/global-styles.js
@@ -2,31 +2,11 @@ import { injectGlobal } from 'styled-components';
 
 /* eslint no-unused-expressions: 0 */
 injectGlobal`
-  html,
-  body {
-    height: 100%;
-    width: 100%;
-  }
+// Global Styles here
 
-  body {
-    font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
-  }
-
-  body.fontLoaded {
-    font-family: 'Open Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;
-  }
-
-  #app {
-    background-color: #fafafa;
-    min-height: 100%;
-    min-width: 100%;
-    margin:0;
-    padding:0;
-  }
-
-  p,
-  label {
-    font-family: Georgia, Times, 'Times New Roman', serif;
-    line-height: 1.5em;
-  }
+  //html,
+  //body {
+  //  height: 100%;
+  //  width: 100%;
+  //}
 `;
