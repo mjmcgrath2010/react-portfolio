3689eb1432d95ec59a1fe4bc3a0aaccd74447366
mjmcgrath2010 <mike@mcgrathsolutions.us>
2018-03-27 09:00:52 -0400

    Added updated thumbnails

--- app/components/ProjectThumbnail/index.js
+++ app/components/ProjectThumbnail/index.js
@@ -7,22 +7,24 @@
 import React from 'react';
 import { Image } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
-// import styled from 'styled-components';
+import styled from 'styled-components';
 
 // import { FormattedMessage } from 'react-intl';
 // import messages from './messages';
 
 import PropTypes from 'prop-types';
 
+const StyledImage = styled(Image)`
+  box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.35);
+`;
+
 // eslint-disable-next-line react/prefer-stateless-function
 class ProjectThumbnail extends React.Component {
   render() {
     return (
-      <div className="project-container">
-        <Link to={this.props.projectPage}>
-          <Image src={this.props.thumbnail} size="medium" alt={this.props.altTag} />
-        </Link>
-      </div>
+      <Link to={this.props.projectPage}>
+        <StyledImage src={this.props.thumbnail} alt={this.props.altTag} />
+      </Link>
     );
   }
 }
